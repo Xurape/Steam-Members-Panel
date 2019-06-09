2d18a080b24ec9eaa4dfbcfdd6319ee4e0790cc1
ShutAP <41197101+ShutAP1337@users.noreply.github.com>
2019-06-09 14:25:29 +0100

    Increased portuguese translation
    Thx for the advice @DikaN1337

--- langs/pt.php
+++ langs/pt.php
@@ -24,21 +24,15 @@ define('CONTENT_COMPRAST', 'Cargo');
 define('CONTENT_COMPRADO', 'Pago');
 define('CONTENT_EXPIRADO', 'Expirado');
 define('CONTENT_CANDIDATURAS', 'Candidaturas Staff');
-define('CONTENT_CANDIDATURASTEXTO', 'Queres seres staff? Então faz já o formulário abaixo e de seguida aguarda uma aprovação/recusação de um superior.');
-define('CONTENT_LOGINTEXT', 'Por favor, loga-te com a steam antes de usares o painel!');
+define('CONTENT_CANDIDATURASTEXTO', 'Desejas ter um cargo? Então faz já o formulário abaixo e de seguida aguarda uma aprovação/recusação de um superior.');
+define('CONTENT_LOGINTEXT', 'Por favor, loga-te com a Steam antes de usares o painel!');
 define('CONTENT_STAFFINFO', 'A nossa staff');
 define('CONTENT_PAINELADMIN', 'Painel de Admin');
-define('CONTENT_FUNDADOR', 'Fundador');
-define('CONTENT_COFUNDADOR', 'Co-Fundador');
-define('CONTENT_HEADADMIN', 'Head-Admin');
-define('CONTENT_ADMINISTRADOR', 'Administrador');
-define('CONTENT_MODERADOR', 'Moderador');
-define('CONTENT_HELPER', 'Helper');
 define('CONTENT_YOURNAME', 'O teu nome');
-define('CONTENT_YOURSTEAM', 'O teu link da steam');
+define('CONTENT_YOURSTEAM', 'O teu link da Steam');
 define('CONTENT_YOUREMAIL', 'O teu email');
 define('CONTENT_CARGO', 'Para que cargo desejas ir');
-define('CONTENT_KNOWLEGDGE', 'Que conhecimentos tens');
+define('CONTENT_KNOWLEGDGE', 'Qual o teu conhecimento');
 define('CONTENT_ENVIAR', 'Enviar');
 define('CONTENT_SOON', 'BREVE');
 
@@ -50,6 +44,12 @@ define('CONTENT_SOON', 'BREVE');
 */
 
 define('CONTENT_DEVELOPER', 'Desenvolvedor');
+define('CONTENT_FUNDADOR', 'Fundador');
+define('CONTENT_COFUNDADOR', 'Co-Fundador');
+define('CONTENT_HEADADMIN', 'Head-Admin');
+define('CONTENT_ADMINISTRADOR', 'Administrador');
+define('CONTENT_MODERADOR', 'Moderador');
+define('CONTENT_HELPER', 'Helper');
 define('CONTENT_MEMBER', 'Membro');
 
 ?>
