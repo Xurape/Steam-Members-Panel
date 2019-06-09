34a24da8d57c6186ebba37d5200286092306df62
DikaN <diogomiguelcandido@gmail.com>
2019-06-09 01:40:39 +0100

    Using $linguagempredefinida.
    When ?l= is not defined and if $linguagempredefinida is PT default language will be Portuguese, also when EN is the default it will be English.

--- langs/lang.php
+++ langs/lang.php
@@ -1,5 +1,7 @@
 <?php
 
+include '../includes/config.php';
+
 function setLanguage()
 {
     if(isset($_GET['l']))
@@ -15,7 +17,18 @@ function setLanguage()
     }
     else
     {
-        require_once('langs/pt.php');
+        if($linguagempredefinida === PT)
+        {
+            require_once('langs/pt.php');
+        }
+        elseif($linguagempredefinida === EN)
+        {
+            require_once('langs/en.php');
+        }
+        else
+        {
+            require_once('langs/en.php'); // BECAUSE DONT HAVE LANGUAGE SO ENGLISH NEED TO BE DEFAULT!
+        }
     }
 }
 
