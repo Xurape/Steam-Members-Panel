34deb8c406e6475e316ac0fc12bd923f89098c9e
ShutAP <41197101+ShutAP1337@users.noreply.github.com>
2019-06-09 15:22:08 +0100

    Created russian translation

---
+++ langs/ru.php
@@ -0,0 +1,55 @@
+<?php
+
+define('CONTENT_INICIO', 'первый');
+define('CONTENT_OCUPADO', 'занятый');
+define('CONTENT_AUSENTE', 'отсутствующий');
+define('CONTENT_ADORMIR', 'Спать');
+define('CONTENT_SERVIDORES', 'серверы');
+define('CONTENT_COMPRARVIP', 'Купить VIP');
+define('CONTENT_OFERECERVIP', 'предложение VIP');
+define('CONTENT_CONTA', 'счет');
+define('CONTENT_AMINHACONTA', 'Мой аккаунт');
+define('CONTENT_MUDARDECONTA', 'Изменить аккаунт');
+define('CONTENT_DEFINIÇÕES', 'настройки');
+define('CONTENT_ANOSSAEQUIPA', 'Наша команда');
+define('CONTENT_REGISTEDS', 'Зарегистрированные участники');
+define('CONTENT_REGISTEDST', 'зарегистрированный');
+define('CONTENT_COMPRAS', 'Ваши покупки');
+define('CONTENT_COMPRASNR', 'номер');
+define('CONTENT_COMPRASNM', 'имя');
+define('CONTENT_COMPRASD', 'дата');   
+define('CONTENT_COMPRASDD', 'Следующий месяц оплата'); 
+define('CONTENT_COMPRASS', 'статус');
+define('CONTENT_COMPRAST', 'офис');
+define('CONTENT_COMPRADO', 'оплаченный');
+define('CONTENT_EXPIRADO', 'истекший');
+define('CONTENT_CANDIDATURAS', 'Прикладной персонал');
+define('CONTENT_CANDIDATURASTEXTO', 'Вы хотите иметь должность? Затем заполните форму ниже и затем ждите одобрения / вызова от начальника.');
+define('CONTENT_LOGINTEXT', 'Пожалуйста, авторизуйтесь в Steam перед использованием панели!');
+define('CONTENT_STAFFINFO', 'Наш пар');
+define('CONTENT_PAINELADMIN', 'Панель администратора');
+define('CONTENT_YOURNAME', 'Ваше имя');
+define('CONTENT_YOURSTEAM', 'Ваша ссылка в Steam');
+define('CONTENT_YOUREMAIL', 'Ваш электронный адрес');
+define('CONTENT_CARGO', 'За какой заряд ты хочешь пойти?');
+define('CONTENT_KNOWLEGDGE', 'Каковы ваши знания?');
+define('CONTENT_ENVIAR', 'послать');
+define('CONTENT_SOON', 'КРАТКАЯ');
+
+/*
+
+          ПОЗИЦИИ
+      ПЕРЕВОД СБОРОВ
+
+*/
+
+define('CONTENT_DEVELOPER', 'разработчик');
+define('CONTENT_FUNDADOR', 'основатель');
+define('CONTENT_COFUNDADOR', 'Соучредитель');
+define('CONTENT_HEADADMIN', 'Руководитель-Admin');
+define('CONTENT_ADMINISTRADOR', 'администратор');
+define('CONTENT_MODERADOR', 'Просмотр сообщений на форуме');
+define('CONTENT_HELPER', 'помощник');
+define('CONTENT_MEMBER', 'член');
+
+?>
