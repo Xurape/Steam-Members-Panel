a78ee73af65e2c762db183a32cdb90f75a838c36
DikaN <diogomiguelcandido@gmail.com>
2019-06-09 02:47:53 +0100

    Updated new languages

--- langs/lang.php
+++ langs/lang.php
@@ -14,6 +14,18 @@ function setLanguage()
         {
             require_once('langs/en.php');
         }
+        elseif($_GET['l'] == 'es')
+        {
+            require_once('langs/es.php');
+        }
+        elseif($_GET['l'] == 'fr')
+        {
+            require_once('langs/fr.php');
+        }
+        elseif($_GET['l'] == 'ru')
+        {
+            require_once('langs/ru.php');
+        }
     }
     else
     {
@@ -25,9 +37,21 @@ function setLanguage()
         {
             require_once('langs/en.php');
         }
+        elseif($linguagempredefinida === ES)
+        {
+            require_once('langs/es.php');
+        }
+        elseif($linguagempredefinida === FR)
+        {
+            require_once('langs/fr.php');
+        }
+        elseif($linguagempredefinida === RU)
+        {
+            require_once('langs/ru.php');
+        }
         else
         {
-            require_once('langs/en.php'); // BECAUSE DONT HAVE LANGUAGE SO ENGLISH NEED TO BE DEFAULT!
+            require_once('langs/en.php'); // DON'T HAVE LANGUAGE SO ENGLISH NEED TO BE DEFAULT!
         }
     }
 }
