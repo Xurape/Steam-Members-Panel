e07e27a2274cb34082a79376ddaf6b51c6d2ea12
DikaN <diogomiguelcandido@gmail.com>
2019-06-09 03:04:32 +0100

    Spanish Language Added

--- langs/es.php
+++ langs/es.php
@@ -1,1 +1,49 @@
-
+<?php
+define('CONTENT_INICIO', 'Primero');
+define('CONTENT_OCUPADO', 'Ocupado');
+define('CONTENT_AUSENTE', 'Ausente');
+define('CONTENT_ADORMIR', 'A dormir');
+define('CONTENT_SERVIDORES', 'Servidores');
+define('CONTENT_COMPRARVIP', 'Comprar VIP');
+define('CONTENT_OFERECERVIP', 'Oferta VIP');
+define('CONTENT_CONTA', 'Cuenta');
+define('CONTENT_AMINHACONTA', 'Mi cuenta');
+define('CONTENT_MUDARDECONTA', 'Cambiar de cuenta');
+define('CONTENT_DEFINIÇÕES', 'Ajustes');
+define('CONTENT_ANOSSAEQUIPA', 'Nuestro equipo');
+define('CONTENT_REGISTEDS', 'Miembros Registrados');
+define('CONTENT_REGISTEDST', 'Registrados');
+define('CONTENT_COMPRAS', 'Sus compras');
+define('CONTENT_COMPRASNR', 'Número');
+define('CONTENT_COMPRASNM', 'Nombre');
+define('CONTENT_COMPRASD', 'Fecha');   
+define('CONTENT_COMPRASDD', 'Siguiente pago'); 
+define('CONTENT_COMPRASS', 'Status');
+define('CONTENT_COMPRAST', 'Estatus');
+define('CONTENT_COMPRADO', 'Pagodo');
+define('CONTENT_EXPIRADO', 'Respirado');
+define('CONTENT_CANDIDATURAS', 'Solicitud de personal');
+define('CONTENT_CANDIDATURASTEXTO', '¿Quieres ser personal? Entonces hace ya el formulario abajo y después aguarda una aprobación / rechazo de un superior.');
+define('CONTENT_LOGINTEXT', 'Por favor, lográndose con el Steam antes de usar el panel!');
+define('CONTENT_STAFFINFO', 'Nuestro personal');
+define('CONTENT_PAINELADMIN', 'Panel de administración');
+define('CONTENT_FUNDADOR', 'Fundador');
+define('CONTENT_COFUNDADOR', 'Co-Fundador');
+define('CONTENT_HEADADMIN', 'Head-Admin');
+define('CONTENT_ADMINISTRADOR', 'Administrador');
+define('CONTENT_MODERADOR', 'Moderador');
+define('CONTENT_HELPER', 'Helper');
+define('CONTENT_YOURNAME', 'Tu nombre');
+define('CONTENT_YOURSTEAM', 'Tu enlace de steam');
+define('CONTENT_YOUREMAIL', 'Tu correo electrónico');
+define('CONTENT_CARGO', '¿Para qué cargo deseas ir?');
+define('CONTENT_KNOWLEGDGE', '¿Qué conocimientos tienes?');
+define('CONTENT_ENVIAR', 'Enviar');
+define('CONTENT_SOON', 'CORTO');
+/*
+          CARGOS
+    TRADUÇÃO DOS CARGOS
+*/
+define('CONTENT_DEVELOPER', 'Desenvolvedor');
+define('CONTENT_MEMBER', 'Membro');
+?>
